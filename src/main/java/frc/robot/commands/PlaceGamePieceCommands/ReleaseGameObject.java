ec346b6080bfa6286541321bdb6bd8e35175d4fa
Hydro111 <hydrogenhone@gmail.com>
2023-01-31 19:50:06 -0700

    Begin ReleaseGameObject work

--- src/main/java/frc/robot/commands/PlaceGamePieceCommands/ReleaseGameObject.java
+++ src/main/java/frc/robot/commands/PlaceGamePieceCommands/ReleaseGameObject.java
@@ -9,9 +9,17 @@ import frc.robot.Constants;
 import frc.robot.subsystems.ReidPrototypeSubsystem;
 
 public class ReleaseGameObject extends CommandBase {
+  private boolean isCube;
+  private ReidPrototypeSubsystem grabberSubsystem;
+  private Constants.Target target;
+
   /** Creates a new ReleaseGameObject. */
   public ReleaseGameObject(boolean isCube, Constants.Target target, ReidPrototypeSubsystem grabberSubsystem) {
+    this.isCube = isCube;
+    this.grabberSubsystem = grabberSubsystem;
+    this.target = target;
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(grabberSubsystem);
   }
 
   // Called when the command is initially scheduled.
@@ -21,7 +29,7 @@ public class ReleaseGameObject extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    
+    grabberSubsystem.openGrabber(0.1); // H! TODO Put a speed that makes sense in here
   }
 
   // Called once the command ends or is interrupted.
@@ -31,6 +39,6 @@ public class ReleaseGameObject extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return false; // H! TODO Implement finding out whether the grabber is open and make this dependent on it
   }
 }
