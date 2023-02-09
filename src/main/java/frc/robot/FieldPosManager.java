e089d10363cbbc2e1934f986d43f3e723d8382d0
May0san <maceface999@gmail.com>
2023-02-09 16:52:36 -0700

    Update FieldPosManager.java

--- src/main/java/frc/robot/FieldPosManager.java
+++ src/main/java/frc/robot/FieldPosManager.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.wpilibj.DriverStation;
 
 /** ++ This class manages the field locations for the robot. It'll deal with outputs from PhotonVisioin. 
@@ -14,7 +15,7 @@ public class FieldPosManager {
 
     public DriverStation.Alliance allianceColor = DriverStation.getAlliance();
 
-    public Pose2d lastRobotPosition = new Pose2d();
+    public Pose2d latestRobotPosition = new Pose2d();
 
     // ++ this value is the current target target for the robot scoring position
     public int currentIndex;
@@ -37,6 +38,17 @@ public class FieldPosManager {
     public FieldPosManager(){
         
     }
+
+    public void setRobotPose(Pose2d pose){
+        latestRobotPosition = pose;
+    }
+
+    public void incrementRobotPose(Transform2d deltaPose){
+        latestRobotPosition.transformBy(deltaPose);
+    }
     
+    public Pose2d getRobotPose(){
+        return latestRobotPosition;
+    }
 
 }
