a32318482859d1aa9d866c0940a86b9492adcce3
Hydro111 <hydrogenhone@gmail.com>
2023-02-11 17:51:39 -0700

    Modify PlaceGamePiece for integration with FieldPositionManager

--- src/main/java/frc/robot/commands/PlaceGamePiece.java
+++ src/main/java/frc/robot/commands/PlaceGamePiece.java
@@ -5,8 +5,10 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.wpilibj.event.EventLoop;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
+import frc.robot.JoyUtil;
 import frc.robot.commands.PlaceGamePieceCommands.MoveArmToTarget;
 import frc.robot.commands.PlaceGamePieceCommands.MoveRobotToGrid;
 import frc.robot.commands.PlaceGamePieceCommands.ReleaseGameObject;
@@ -21,15 +23,55 @@ public class PlaceGamePiece extends SequentialCommandGroup {
 
   public boolean isCube;
   public Constants.Target target;
+  public DriveSubsystem driveSubsystem;
+  public LegAnkleSubsystem legAnkleSubsystem;
+  public ReidPrototypeSubsystem grabberSubsystem;
+  public ProfiledPIDController thetaPidController;
+  public JoyUtil controller;
+
+  private EventLoop rightPOVBindingEventLoop;
+  private EventLoop leftPOVBindingEventLoop;
 
   /** Creates a new PlaceGamePiece. */
-  public PlaceGamePiece(DriveSubsystem driveSubsystem, LegAnkleSubsystem legAnkleSubsystem, ReidPrototypeSubsystem grabberSubsystem, ProfiledPIDController thetaPidController) {
+  public PlaceGamePiece(DriveSubsystem driveSubsystem, LegAnkleSubsystem legAnkleSubsystem, ReidPrototypeSubsystem grabberSubsystem, ProfiledPIDController thetaPidController, JoyUtil controller, int poseIndex) {
+    this.driveSubsystem = driveSubsystem;
+    this.legAnkleSubsystem = legAnkleSubsystem;
+    this.grabberSubsystem = grabberSubsystem;
+    this.thetaPidController = thetaPidController;
+    this.controller = controller;
+    
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands(
-      new MoveRobotToGrid(isCube, target, driveSubsystem, null, thetaPidController),
+    addCommands( // H! TODO Make the target pose based on the index passed in and the FieldPositionManager pose array
+      new MoveRobotToGrid(null, driveSubsystem, controller, thetaPidController),
       new MoveArmToTarget(isCube, target, legAnkleSubsystem),
       new ReleaseGameObject(isCube, target, grabberSubsystem)
     );
+
+    
+    // H! Binds onPOVRight to starting pov right
+    rightPOVBindingEventLoop = new EventLoop();
+    controller.povRight(rightPOVBindingEventLoop).rising().ifHigh(this::onPOVRight);
+
+    // H! Binds onPOVLeft to starting pov left
+    leftPOVBindingEventLoop = new EventLoop();
+    controller.povLeft(leftPOVBindingEventLoop).rising().ifHigh(this::onPOVLeft);
+  }
+
+
+
+  public PlaceGamePiece(DriveSubsystem driveSubsystem, LegAnkleSubsystem legAnkleSubsystem, ReidPrototypeSubsystem grabberSubsystem, ProfiledPIDController thetaPidController, JoyUtil controller) {
+    this(driveSubsystem, legAnkleSubsystem, grabberSubsystem, thetaPidController, controller, -1 /* H! TODO Needs to be integrated with finding the closest pose */);
+  }
+
+
+  public void onPOVRight() {
+    this.cancel();
+    new PlaceGamePiece(driveSubsystem, legAnkleSubsystem, grabberSubsystem, thetaPidController, controller).schedule();
+  }
+  
+  public void onPOVLeft() {
+    this.cancel();
+    new PlaceGamePiece(driveSubsystem, legAnkleSubsystem, grabberSubsystem, thetaPidController, controller).schedule();
   }
 }
--- src/main/java/frc/robot/commands/PlaceGamePieceCommands/MoveRobotToGrid.java
+++ src/main/java/frc/robot/commands/PlaceGamePieceCommands/MoveRobotToGrid.java
@@ -6,99 +6,34 @@ package frc.robot.commands.PlaceGamePieceCommands;
 
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.wpilibj.event.BooleanEvent;
-import edu.wpi.first.wpilibj.event.EventLoop;
-import frc.robot.Constants;
 import frc.robot.JoyUtil;
 import frc.robot.commands.SwerveAutoMoveCommand;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class MoveRobotToGrid extends SwerveAutoMoveCommand {
-  private boolean isCube;
   private DriveSubsystem driveSubsystem;
-  private Constants.Target target;
   private JoyUtil controller;
-  private EventLoop rightPOVBindingEventLoop;
-  private EventLoop leftPOVBindingEventLoop;
-  private int targetPoseIndex;
-  private boolean isDone;
   private ProfiledPIDController thetaPidController;
 
   /** Creates a new MoveRobotToGrid. */
-  public MoveRobotToGrid(boolean isCube, Constants.Target target, DriveSubsystem driveSubsystem, JoyUtil controller, ProfiledPIDController thetaPidController) {
+  public MoveRobotToGrid(Pose2d targetPose, DriveSubsystem driveSubsystem, JoyUtil controller, ProfiledPIDController thetaPidController) {
     super(
       driveSubsystem,
-      new Pose2d(), // H! TODO Make this actually be the correct pose
+      targetPose,
       thetaPidController,
       false
     );
-    this.isCube = isCube;
     this.driveSubsystem = driveSubsystem;
-    this.target = target; 
     this.controller = controller;
     this.thetaPidController = thetaPidController;
-
-    // H! Binds onPOVRight to starting pov right
-    rightPOVBindingEventLoop = new EventLoop();
-    controller.povRight(rightPOVBindingEventLoop).rising().ifHigh(this::onPOVRight);
-
-    // H! Binds onPOVLeft to starting pov left
-    leftPOVBindingEventLoop = new EventLoop();
-    controller.povLeft(leftPOVBindingEventLoop).rising().ifHigh(this::onPOVLeft);
   }
 
-
-  /** Creates a new MoveRobotToGrid. */
-  public MoveRobotToGrid(boolean isCube, Constants.Target target, DriveSubsystem driveSubsystem, JoyUtil controller, ProfiledPIDController thetaPidController, int targetPoseIndex) {
-    super(
-      driveSubsystem,
-      new Pose2d(), // H! TODO Make this actually be the correct pose
-      thetaPidController,
-      false
-    );
-    this.isCube = isCube;
-    this.driveSubsystem = driveSubsystem;
-    this.target = target; 
-    this.controller = controller;
-    this.thetaPidController = thetaPidController;
-    this.targetPoseIndex = targetPoseIndex;
-
-    // H! Binds onPOVRight to starting pov right
-    rightPOVBindingEventLoop = new EventLoop();
-    controller.povRight(rightPOVBindingEventLoop).rising().ifHigh(this::onPOVRight);
-
-    // H! Binds onPOVLeft to starting pov left
-    leftPOVBindingEventLoop = new EventLoop();
-    controller.povLeft(leftPOVBindingEventLoop).rising().ifHigh(this::onPOVLeft);
-  }
-
-
-
-  @Override
-  public void execute() {
-    super.execute();
-
-    // H! WPI Lib says to do this
-    rightPOVBindingEventLoop.poll();
-    leftPOVBindingEventLoop.poll();
-  }
 
 
   @Override
   public boolean isFinished() {
-    if (isDone) {return true;}
-    return super.isFinished();
-  }
-
-
-  public void onPOVRight() {
-    isDone = true;
-    new MoveRobotToGrid(isCube, target, driveSubsystem, controller, thetaPidController, targetPoseIndex + 1).schedule();
-  }
-  
-  public void onPOVLeft() {
-    isDone = true;
-    new MoveRobotToGrid(isCube, target, driveSubsystem, controller, thetaPidController, targetPoseIndex - 1).schedule();
+    // H! Only end the command if the a button is pressed to confirm it
+    return super.isFinished() & controller.getAButton();
   }
   
   
