0c1905700d7de6c62e8eb3d40439a48e7e2d48ca
May0san <maceface999@gmail.com>
2023-02-24 17:02:07 -0700

    mleen
    did some PID tuning, added an output range on pivot, also made the A button stop applying setpoints to PIDs, so we can test setpoints over long distances

--- src/main/java/frc/robot/commands/WristCommand.java
+++ src/main/java/frc/robot/commands/WristCommand.java
@@ -28,11 +28,11 @@ public class WristCommand extends CommandBase {
   @Override
   public void execute() {
     //m_subsystem.moveByXYTheta(JoyUtil.posWithDeadzone( m_controller.getLeftX() ), JoyUtil.posWithDeadzone( -m_controller.getLeftY() ), JoyUtil.posWithDeadzone( m_controller.getRightY() ), JoyUtil.posWithDeadzone( -m_controller.getRightX()));
-    m_subsystem.moveManualSetpoints(JoyUtil.posWithDeadzone( m_controller.getLeftX() ), JoyUtil.posWithDeadzone( -m_controller.getLeftY() ), JoyUtil.posWithDeadzone( m_controller.getRightY() ), JoyUtil.posWithDeadzone( -m_controller.getRightX()));
     
-    if (m_controller.getAButton()) {
-      m_subsystem.resetRoll();
-    }
+    m_subsystem.moveManualSetpoints(JoyUtil.posWithDeadzone( m_controller.getLeftX() )/5, JoyUtil.posWithDeadzone( -m_controller.getLeftY() ), JoyUtil.posWithDeadzone( m_controller.getRightY() ), JoyUtil.posWithDeadzone( -m_controller.getRightX()));
+    
+    m_subsystem.deleteThis_doSetpoint=!m_controller.getAButton();
+      //m_subsystem.resetRoll();
   }
 
   // Called once the command ends or is interrupted.
