1dc7c51139ad66a1d39060404b92d19f87825467
Blockinator1 <70295628+Blockinator1@users.noreply.github.com>
2023-03-09 14:38:08 -0700

    Added import to remove error

--- src/main/java/frc/robot/subsystems/LegAnkleSubsystem.java
+++ src/main/java/frc/robot/subsystems/LegAnkleSubsystem.java
@@ -6,6 +6,7 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxAbsoluteEncoder;
 import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.networktables.GenericEntry;
