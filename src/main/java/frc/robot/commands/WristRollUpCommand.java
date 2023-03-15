84febb3ccb56cec5d58ea29832b1d34fb6578b4d
Mothandman <117953018+Mothandman@users.noreply.github.com>
2023-03-14 18:01:58 -0600

    Make the thing that rolls the wrist 180 degrees with a button

---
+++ src/main/java/frc/robot/commands/WristRollUpCommand.java
@@ -0,0 +1,10 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.LegAnkleSubsystem;
+
+public class WristRollUpCommand extends MoveLegAnkleToPartialPositionCommand {
+
+    public WristRollUpCommand(LegAnkleSubsystem legAnkleSubsystem) {
+       super(legAnkleSubsystem, null, null, null, .5);
+    }   
+}
