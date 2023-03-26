4def641bfa6233f46366403191d19819a469a67a
Meivuu <70550220+May0san@users.noreply.github.com>
2023-03-26 13:54:35 -0600

    made roll commands

---
+++ src/main/java/frc/robot/commands/Autonomous/WristRollDefaultAuto.java
@@ -0,0 +1,14 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Autonomous;
+
+import frc.robot.commands.LegAnkle.WristRollDefaultCommand;
+import frc.robot.subsystems.LegAnkleSubsystem;
+
+public class WristRollDefaultAuto extends WristRollDefaultCommand {
+  WristRollDefaultAuto(LegAnkleSubsystem legAnkleSubsystem) {
+    super(legAnkleSubsystem);
+  }
+}
